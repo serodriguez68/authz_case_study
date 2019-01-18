c01a5c3bac7a7e20b7b62ed5f4985b6fa35a94ac
Sergio Rodriguez <se.rodriguez68@gmail.com>
2019-01-18 13:11:40 +1100

    make seed files identical between start and master

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,42 +1,44 @@
 # Constants
 PWD = "123456789"
 
+
 # Cities
 mel = City.create!(name: 'Melbourne')
 syd = City.create!(name: 'Sydney')
 
-# Clients
+# Users
+# - Clients
 (1..10).each do |num|
   u = User.create!(email: "client#{num}@e.com", password: PWD, password_confirmation: PWD)
   Client.create!(name: "Client#{num}", user: u)
 end
 
-# Drivers
+# - Drivers
 (1..10).each do |num|
   u = User.create!(email: "driver#{num}@e.com", password: PWD, password_confirmation: PWD)
   Driver.create!(name: "Driver#{num}", user: u)
 end
 
-# Staff members
-# city analyst
-mel_ca = User.create!(email: "mel_ca@e.com", password: PWD, password_confirmation: PWD)
-StaffMember.create!(name: Faker::StarWars.character, user: mel_ca)
-syd_ca = User.create!(email: "syd_ca@e.com", password: PWD, password_confirmation: PWD)
-StaffMember.create!(name: Faker::StarWars.character, user: syd_ca)
+# - Staff members
+# -- city analyst
+user_mel_ca = User.create!(email: "mel_ca@e.com", password: PWD, password_confirmation: PWD)
+StaffMember.create!(name: Faker::StarWars.character, user: user_mel_ca)
+user_syd_ca = User.create!(email: "syd_ca@e.com", password: PWD, password_confirmation: PWD)
+StaffMember.create!(name: Faker::StarWars.character, user: user_syd_ca)
 
-# city managers
-mel_cm = User.create!(email: "mel_cm@e.com", password: PWD, password_confirmation: PWD)
-StaffMember.create!(name: Faker::StarWars.character, user: mel_cm)
-syd_cm = User.create!(email: "syd_cm@e.com", password: PWD, password_confirmation: PWD)
-StaffMember.create!(name: Faker::StarWars.character, user: syd_cm)
+# -- city managers
+user_mel_cm = User.create!(email: "mel_cm@e.com", password: PWD, password_confirmation: PWD)
+StaffMember.create!(name: Faker::StarWars.character, user: user_mel_cm)
+user_syd_cm = User.create!(email: "syd_cm@e.com", password: PWD, password_confirmation: PWD)
+StaffMember.create!(name: Faker::StarWars.character, user: user_syd_cm)
 
-# general managers
-gm = User.create!(email: "gm@e.com", password: PWD, password_confirmation: PWD)
-StaffMember.create!(name: Faker::StarWars.character, user: gm)
+# -- general managers
+user_gm = User.create!(email: "gm@e.com", password: PWD, password_confirmation: PWD)
+StaffMember.create!(name: Faker::StarWars.character, user: user_gm)
 
-# Authorization Admins
-aa = User.create!(email: "aa@e.com", password: PWD, password_confirmation: PWD)
-StaffMember.create!(name: Faker::StarWars.character, user: aa)
+# -- Authorization Admins
+user_aa = User.create!(email: "aa@e.com", password: PWD, password_confirmation: PWD)
+StaffMember.create!(name: Faker::StarWars.character, user: user_aa)
 
 # Services
 City.all.each do |city|
